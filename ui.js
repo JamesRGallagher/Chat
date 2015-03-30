b44be145c4b33cb7e75ff72a8f7f7074a1e2a36f
James Gallagher <jamesr.gallagher1@gmail.com>
2015-03-30 14:16:09 +0100

    change
    s

--- ui.js
+++ ui.js
@@ -2,15 +2,16 @@
 * UI lib
 * Deals with the UI of the chat.
 */
+var SlideOut = (function() {  
 var mainWindow = {
-    messageEntry:$('#messageTextBox');
-    chatArea: $('#commentArea');
+    messageEntry:$('#messageTextBox'),
+    chatArea: $('#commentArea')
 }
 
 var slideOutMenu = {
-    pageHolder:$('#menuHolder');
-    updateMyInfo:$('#updateButton');
-    usersInRoom:$('#usersInRoomHolder');
-    threads:$('#threadsMenuOption');
+    pageHolder:$('#menuHolder'),
+    updateMyInfo:$('#updateButton'),
+    usersInRoom:$('#usersInRoomHolder'),
+    threads:$('#threadsMenuOption')
 }
-
+})();

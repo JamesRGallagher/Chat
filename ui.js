d6254cb8b8aa0973957919261bdced8e85e4acdf
James Gallagher <jamesr.gallagher1@gmail.com>
2015-04-09 15:52:43 +0100

    Refactor

--- ui.js
+++ ui.js
@@ -24,3 +24,23 @@ getSlideOutMenu = function(){
 
 }
 }) ();
+
+/**
+ * slideOut 
+ * Contains functions to deal with page 
+ * and history managment with the slideout
+ * menu
+ */
+var SlideOut = (function() {  
+	console.log('slider')
+	var slider = UI.getSlideOutMenu().pageHolder;  
+	function newSliderPage(html){
+	    current = slider.html();
+	    backstack.push(current);
+	    slider.html('');
+	    slider.html(html);   
+	}  return {
+		newSliderPage:newSliderPage
+	}
+
+})();
